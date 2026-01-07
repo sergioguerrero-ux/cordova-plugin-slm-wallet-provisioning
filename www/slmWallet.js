0ff9f645309baa011ecb31b3aba42d0c6c772812
Sergio Guerrero <sergio.guerrero@slm.cloud>
2026-01-06 18:43:18 -0600

    refactor: Rename plugin identifier to `SLMWallet` and update Android package path and JavaScript service calls.

--- www/slmWallet.js
+++ www/slmWallet.js
@@ -3,27 +3,15 @@ var exec = require('cordova/exec');
 
 module.exports = {
     appleCanAdd: function (success, error) {
-        exec(success, error, 'SlmWalletProvisioning', 'appleCanAdd', []);
+        exec(success, error, 'SLMWallet', 'appleCanAdd', []);
     },
-    /**
-     * opts:
-     *   cardholderName, last4, description,
-     *   cardId                          // required
-     *   tokenizationEndpoint            // overrides https://api.pomelo.la/token-provisioning/mastercard/apple-pay
-     *   tokenizationAuthorization       // value for Authorization header
-     *   tokenizationAuthToken,          // alternative: token + optional tokenizationAuthScheme (default: "Bearer")
-     *   tokenizationHeaders             // additional headers
-     *   userId                          // optional Pomelo user_id
-     * See https://developers.pomelo.la/api-reference/cards/Tokenization/mastercard#aprovisionar-mastercard-en-apple-pay
-     */
     appleStartAdd: function (opts, success, error) {
-        exec(success, error, 'SlmWalletProvisioning', 'appleStartAdd', [opts || {}]);
+        exec(success, error, 'SLMWallet', 'appleStartAdd', [opts || {}]);
     },
-
     googleIsAvailable: function (success, error) {
-        exec(success, error, 'SlmWalletProvisioning', 'googleIsAvailable', []);
+        exec(success, error, 'SLMWallet', 'googleIsAvailable', []);
     },
     googlePushProvision: function (opts, success, error) {
-        exec(success, error, 'SlmWalletProvisioning', 'googlePushProvision', [opts || {}]);
+        exec(success, error, 'SLMWallet', 'googlePushProvision', [opts || {}]);
     }
 };
